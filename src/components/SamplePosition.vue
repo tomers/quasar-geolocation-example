e5bae5f52ec0740fb7636ca834bb8890705f523f
Tomer Shalev <tshalev@proofpoint.com>
2020-03-05 23:10:43 +0200

    Initial Google Maps support

--- src/components/SamplePosition.vue
+++ src/components/SamplePosition.vue
@@ -1,6 +1,20 @@
 <template>
   <div v-if="position">
-    Sample! {{ position }}
+    <GmapMap
+      :center="{lat:10, lng:10}"
+      :zoom="7"
+      map-type-id="terrain"
+      style="width: 500px; height: 300px"
+    >
+      <GmapMarker
+        v-for="(m, index) in markers"
+        :key="index"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center=m.position"
+      />
+    </GmapMap>
   </div>
 </template>
 
@@ -9,7 +23,9 @@ import { mapActions, mapState } from 'vuex'
 
 export default {
   data () {
-    return {}
+    return {
+      markers: []
+    }
   },
   computed: {
     ...mapState('positioning', [

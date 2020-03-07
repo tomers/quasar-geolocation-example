6afd5cf9b16ae713435e6903c49c4968c3f9ee9b
Tomer Shalev <tshalev@proofpoint.com>
2020-03-07 20:16:06 +0200

    GeolocationPermissionBanner: Improve look

--- src/components/GeolocationPermissionBanner.vue
+++ src/components/GeolocationPermissionBanner.vue
@@ -1,41 +1,27 @@
 <template>
-  <div>
-    <q-banner
-      v-if="getterIsPermissionGranted"
-      rounded
-      class="bg-primary text-white"
-    >
-      Geolocation privileges granted
-    </q-banner>
-    <q-banner
-      v-if="getterIsPermissionPrompt"
-      rounded
-      class="bg-primary text-white"
-    >
-      <template v-slot:avatar>
-        <q-icon
-          name="info"
-          color="white"
-        />
-      </template>
-      Need to ask for geolocation privileges
-    </q-banner>
-    <q-banner
+  <q-banner
+    rounded
+    :class="classes"
+  >
+    <template v-slot:avatar>
+      <q-icon
+        :name="icon"
+        :color="iconColor"
+      />
+    </template>
+    {{ label }}
+    <template
       v-if="getterIsPermissionDenied"
-      rounded
-      class="text-white bg-red"
+      v-slot:action
     >
-      Location sharing is blocked
-      <template v-slot:action>
-        <q-btn
-          flat
-          color="white"
-          label="How to share your location"
-          @click="openURL('https://support.google.com/chrome/answer/142065')"
-        />
-      </template>
-    </q-banner>
-  </div>
+      <q-btn
+        flat
+        color="white"
+        label="How to share your location"
+        @click="openURL('https://support.google.com/chrome/answer/142065')"
+      />
+    </template>
+  </q-banner>
 </template>
 
 <script>
@@ -43,6 +29,46 @@ import { mapGetters } from 'vuex'
 
 export default {
   computed: {
+    classes () {
+      if (this.getterIsPermissionGranted) {
+        return 'bg-primary text-white'
+      } else if (this.getterIsPermissionPrompt) {
+        return 'bg-grey-3'
+      } else if (this.getterIsPermissionDenied) {
+        return 'text-white bg-red'
+      }
+      return null
+    },
+    icon () {
+      if (this.getterIsPermissionGranted) {
+        return 'done'
+      } else if (this.getterIsPermissionPrompt) {
+        return 'info'
+      } else if (this.getterIsPermissionDenied) {
+        return 'block'
+      }
+      return null
+    },
+    iconColor () {
+      if (this.getterIsPermissionGranted) {
+        return 'white'
+      } else if (this.getterIsPermissionPrompt) {
+        return null
+      } else if (this.getterIsPermissionDenied) {
+        return 'white'
+      }
+      return null
+    },
+    label () {
+      if (this.getterIsPermissionGranted) {
+        return 'Geolocation privileges granted'
+      } else if (this.getterIsPermissionPrompt) {
+        return 'Need to ask for geolocation privileges'
+      } else if (this.getterIsPermissionDenied) {
+        return 'Location sharing is blocked'
+      }
+      return null
+    },
     ...mapGetters('geolocation', [
       'getterIsPermissionGranted',
       'getterIsPermissionPrompt',
